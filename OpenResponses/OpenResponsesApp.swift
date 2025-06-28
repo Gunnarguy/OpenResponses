da49ad09905b9cea04b382cf825bcec6dc2dcb05
gunnarguy <gunnarguy@me.com>
2025-06-27 22:24:31 -0700

    Initial Commit

---
+++ OpenResponses/OpenResponsesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenResponsesApp.swift
+//  OpenResponses
+//
+//  Created by Gunnar Hostetler on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenResponsesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
